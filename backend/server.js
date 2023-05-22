a6d831411fa67611904c5ca1087dff6daababbfc
Bentalha Aymen <>
2023-05-22 12:24:05 +0200

    GET pour simuler sur le front si ça fonctionne

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,9 @@ const port = 5000;
 
 const app = express();
 
+app.get("/post", (req, res) => {
+    res.json({ message: "Voici les données" });
+});
+
+//Start Server
 app.listen(port, () => console.log("Le Serveur a démarré au port " + port));
