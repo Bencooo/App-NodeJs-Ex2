594d92285468ec6c114b2f6d9bcb93c960466ecd
Bentalha Aymen <>
2023-05-22 18:15:06 +0200

    no message

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,17 +1,10 @@
 const express = require('express');
+const { setPosts, getPosts, editPost } = require('../controllers/post.controller');
 const router = express.Router(); 
 
-router.get("/", (req, res) => {
-    res.json({ message: "Voici les données" });
-});
-
-router.post("/", (req, res) => {
-    res.json({ message: req.body.message });
-});
-
-router.put('/:id', (req, res) => {
-    res.json({ messageId: req.params.id});
-});
+router.get("/", getPosts);
+router.post("/", setPosts);
+router.put('/:id', editPost);
 
 router.delete('/:id', (req, res) => {
     res.json({ message: "post supprimé " + req.params.id});
