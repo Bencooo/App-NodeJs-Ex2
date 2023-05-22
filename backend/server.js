ee2c13f40daa6ac92f7a7cd7b2f36410b4dfac04
Bentalha Aymen <>
2023-05-22 12:44:35 +0200

    Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const port = 5000;
 
 const app = express();
 
-app.get("/post", (req, res) => {
-    res.json({ message: "Voici les données" });
-});
+//Middleware "Resquest"
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+
+app.use("/post", require("./routes/post.routes"));
 
 //Start Server
 app.listen(port, () => console.log("Le Serveur a démarré au port " + port));
